8bbef77e66dc43137ccffab2beeaaf3f267a8f5d
Vasja Volin <vasja.volin@gmail.com>
2019-12-22 18:00:54 -0500

    start speccing and building out top-down

--- lib/sudoku_solver.ex
+++ lib/sudoku_solver.ex
@@ -37,4 +37,53 @@ defmodule SudokuSolver do
   def solve(input) do
     input
   end
+
+  def is_valid(board) do
+    rows = get_rows(board)
+    columns = get_columns(board)
+    blocks = get_blocks(board)
+
+    rows_are_valid(rows) and columns_are_valid(columns) and blocks_are_valid(blocks)
+  end
+
+  def rows_are_valid(rows) do
+    true
+  end
+
+  def columns_are_valid(rows) do
+    true
+  end
+
+  def blocks_are_valid(blocks) do
+    true
+  end
+
+  """
+  ### discussion
+  We are going to solve by brute force, essentially. At every step, we are
+  going to look at the next open cell and try a number for that cell. We will
+  evaluate if the value we tried is valid by looking at the board as a whole
+  and determining if it breaks any rules. If it does not, we move on to the next
+  cell. If it does, we will try the next number [1..9]. If we run out of numbers
+  to try that are valid, we will step back one tried cell, consider that
+  invalid, and try from there again.
+
+  The state that we'd like to track is a list, and the elements it contains
+  should have the following information:
+
+  1. the current number that we are trying in that cell
+  2. the location of the cell in the grid
+
+  We need the number being currently tried because we may need to back up and
+  re-evaluate. We need the cell we were looking at so we can properly set up
+  the board.
+
+  We also need to track the initial board. We have the whole array, but
+  we need to see both the x and y but also the value of the element.
+
+  ### elements
+  [x, y], x -> 1..9, y-> 1..9
+
+
+  """
 end
